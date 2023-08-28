7d9c51933d0eea96ed9183e936dab6bbd49dfb2e
Ruchir Thaman <ruchirthaman@Ruchirs-MacBook-Pro.local>
2023-08-28 13:04:33 +0530

    Add configuration as comment

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
+###
+# Sample configuration /usr/local/etc/nginx/nginx.conf
+# events {
+#     worker_connections  1024;
+# }
+#
+# http {
+#     upstream backend { # Weights are adjusted to demonstrate Least Connections
+#         server 127.0.0.1:5001 weight=1; # Default weight is 1 if not specified
+#         server 127.0.0.1:5002 weight=1;
+#         server 127.0.0.1:5003 weight=1;
+#     }
+#
+#     server {
+#         listen 80;
+#         location / {
+#             proxy_pass http://backend;
+#         }
+#     }
+#
+#     include servers/*;
+# }
+###
+
 from flask import Flask
 
 app = Flask(__name__)
