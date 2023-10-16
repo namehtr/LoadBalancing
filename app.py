96214cc11ba8bb828a3776372eaeb5ab24ec9b5c
ruchirthaman <rtheman23@gmail.com>
2023-10-16 12:12:50 +0530

    Add metrics support using timeseries db prometheus

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
-###
-# Sample configuration /usr/local/etc/nginx/nginx.conf
-# events {
-#     worker_connections  1024;
-# }
-#
-# http {
-#     upstream backend { # Weights are adjusted to demonstrate Least Connections
-#         server 127.0.0.1:5001 weight=1; # Default weight is 1 if not specified
-#         server 127.0.0.1:5002 weight=1;
-#         server 127.0.0.1:5003 weight=1;
-#     }
-#
-#     server {
-#         listen 80;
-#         location / {
-#             proxy_pass http://backend;
-#         }
-#     }
-#
-#     include servers/*;
-# }
-###
+from flask import Flask, Response
+from prometheus_client import start_http_server, Counter, generate_latest
+import matplotlib
 
-from flask import Flask
-
+matplotlib.use('Agg')
 app = Flask(__name__)
-counter = 0  # Global counter to track number of requests
-
+c = Counter('my_successes', 'Description of counter')
 @app.route('/', methods=['POST'])
 def increment_counter():
-    global counter
-    counter += 1
-    return f"Server has processed {counter} requests!"
+    c.inc()  # Increment the counter
+    return "Processed requests!"
 
-if __name__ == "__main__":
-    app.run(port=5001)  # Adjust the port for different instances
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    return Response(generate_latest(), content_type='text/plain')
+
+if __name__ == '__main__':
+    start_http_server(5001)  # Start up the server to expose the metrics.
+    app.run(host='0.0.0.0', port=5001)
